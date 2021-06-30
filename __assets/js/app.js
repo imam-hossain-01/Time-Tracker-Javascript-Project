e5354422c58fcc6978c91f4a11a6e72e2a192a16
mdimamhossain0049 <mdimamhossain0049@gmail.com>
2021-06-30 14:45:52 +0600

    Complete Frontend Change

---
+++ __assets/js/app.js
@@ -0,0 +1,9 @@
+/*
+* Todo List App - Javascript Project
+* Author - MD Imam Hossain
+* Contact - https://www.facebook.com/prantoislam.jim.3/
+*           https://www.fiverr.com/imamhossain0049
+*           https://github.com/mdimamhossain0049
+*/
+
+
